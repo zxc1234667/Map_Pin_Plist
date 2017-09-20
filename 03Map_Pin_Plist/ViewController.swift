d3551c6116c2257a7a6fdd470747f38b39eac291
D7702_09 <d7702_09@D7702-09ui-iMac.local>
2017-09-20 14:37:45 +0900

    add pin of 5 and delegate

--- 03Map_Pin_Plist/ViewController.swift
+++ 03Map_Pin_Plist/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
 
     @IBOutlet weak var myMapView: MKMapView!
     
@@ -54,6 +54,9 @@ class ViewController: UIViewController {
                 // 배열에 추가하기
                 annotations.append(annotation)
                 
+                // delegate 적용시키기
+                myMapView.delegate = self
+                
             }
         } else {
             
@@ -64,6 +67,8 @@ class ViewController: UIViewController {
         // showAnnotations = 핀 전체가 나오도록 center를 잡아줌.
         myMapView.showAnnotations(annotations, animated: true)
         myMapView.addAnnotations(annotations)
+        
+        
     }
     
     // 초기 zoom되는 장소 지정 함수
@@ -76,6 +81,91 @@ class ViewController: UIViewController {
         myMapView.setRegion(region, animated: true)
     }
     
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        // pin 재활용
+        let identifier = "MyPin"
+        var  annotationView = myMapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? MKPinAnnotationView
+        
+        if annotationView == nil {
+            annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+            annotationView?.canShowCallout = true
+            
+            // annotation의 title값에 대해서 일치하면 이미지와 calloutAccessory를 실행.
+            if annotation.title! == "부산시민공원" {
+                // 부산시민공원
+                annotationView?.pinTintColor = UIColor.green
+                let leftIconView = UIImageView(frame: CGRect(x: 0, y: 0, width: 53, height: 53))
+                leftIconView.image = UIImage(named:"citizen_logo.png" )
+                annotationView?.leftCalloutAccessoryView = leftIconView
+                
+            } else if annotation.title! == "DIT 동의과학대학교" {
+                // 동의과학대학교
+                annotationView?.pinTintColor = UIColor.red
+                let leftIconView = UIImageView(frame: CGRect(x: 0, y: 0, width: 45, height: 45))
+                leftIconView.image = UIImage(named:"DIT_logo.png" )
+                annotationView?.leftCalloutAccessoryView = leftIconView
+                
+            } else if annotation.title! == "송상현광장" {
+                // 송상현광장
+                annotationView?.pinTintColor = UIColor.blue
+                let leftIconView = UIImageView(frame: CGRect(x: 0, y: 0, width: 45, height: 45))
+                leftIconView.image = UIImage(named:"Songsang.png" )
+                annotationView?.leftCalloutAccessoryView = leftIconView
+                
+            } else if annotation.title! == "부산여자대학교" {
+                // 부산여자대학교
+                annotationView?.pinTintColor = UIColor.yellow
+                let leftIconView = UIImageView(frame: CGRect(x: 0, y: 0, width: 45, height: 45))
+                leftIconView.image = UIImage(named:"Busan_logo.png" )
+                annotationView?.leftCalloutAccessoryView = leftIconView
+            
+            } else if annotation.title! == "양정역" {
+                // 양정역
+                annotationView?.pinTintColor = UIColor.black
+                let leftIconView = UIImageView(frame: CGRect(x: 0, y: 0, width: 45, height: 45))
+                leftIconView.image = UIImage(named:"subway_logo.jpeg" )
+                annotationView?.leftCalloutAccessoryView = leftIconView
+
+            }
+            
+            }else {
+                annotationView?.annotation = annotation
+            }
+        
+        /* 애니메이션 부분 적용 안되서 주석처리
+        
+        //pin에 애니메이션을 적용시킴(위에서 아래로 떨어지는 애니메이션 추가).
+        annotationView?.animatesDrop = true
+        
+        //leftcalloutAcceary
+        annotationView?.canShowCallout = true
+         
+        */
+        let btn = UIButton(type: .detailDisclosure)
+        annotationView?.rightCalloutAccessoryView = btn
+        
+ 
+        
+        return annotationView
+        
+    }
+    
+    // callout accessary를 눌렀을때 alert View 보여줌
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        
+        print("callout Accessory Tapped!")
+        
+        let viewAnno = view.annotation
+        let viewTitle: String = ((viewAnno?.title)!)!
+        let viewSubTitle: String = ((viewAnno?.subtitle)!)!
+        
+        print("\(viewTitle) \(viewSubTitle)")
+        
+        let ac = UIAlertController(title: viewTitle, message: viewSubTitle, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(ac, animated: true, completion: nil)
+    }
 
 }
 
